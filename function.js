15d8016f24d99e70264df089512452507f62282c
hiresfilip <moltres1888@seznam.cz>
2020-01-21 22:12:42 +0100

    prvni

---
+++ function.js
@@ -0,0 +1,58 @@
+/* Tóny pro bílé klapky */
+
+let Cton = new Audio()
+Cton.src = "C.wav";
+
+let Dton = new Audio()
+Dton.src = "D.wav";
+
+let Eton = new Audio()
+Eton.src = "E.wav";
+
+let Fton = new Audio()
+Fton.src = "F.wav";
+
+let Gton = new Audio()
+Gton.src = "G.wav";
+
+let Aton = new Audio()
+Aton.src = "A.wav";
+
+let Bton = new Audio()
+Bton.src = "B.wav";
+
+let C1ton = new Audio()
+C1ton.src = "C1.wav";
+
+let D1ton = new Audio()
+D1ton.src = "D1.wav";
+
+let E1ton = new Audio()
+E1ton.src = "E1.wav";
+
+let F1ton = new Audio()
+F1ton.src = "F1.wav";
+
+
+/* Tóny pro černé klapky*/
+
+let Cston = new Audio()
+Cston.src = "C_s.wav";
+
+let Dston = new Audio()
+Dston.src = "D_s.wav";
+
+let Fston = new Audio()
+Fston.src = "F_s.wav";
+
+let Gston = new Audio()
+Gston.src = "G_s.wav";
+
+let Bbton = new Audio()
+Bbton.src = "Bb.wav";
+
+let Cs1ton = new Audio()
+Cs1ton.src = "C_s1.wav";
+
+let Ds1ton = new Audio()
+Ds1ton.src = "D_s1.wav";
